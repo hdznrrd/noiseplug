64490b52ed144e902e1f13decfe0549ee71b48b8
Joachim Fenkes <github@dojoe.net>
2012-04-08 15:53:25 +0200

    Move a couple things to progmem, fix compiler warning regarding parens

--- sound1.c
+++ sound1.c
@@ -1,14 +1,21 @@
 #include <inttypes.h>
 
+#ifndef PROGMEM
+#define PROGMEM /* nix */
+#endif
+
 #define APREGGIO_RANGE 3
 #define APREGGIO_DELAY 200
 
 #define BIT(x,n) (((x)&(1<<(n)))>>(n))
 #define SO(x) (sizeof((x))/sizeof(*(x)))
 
-static const uint8_t sin[] = {0, 49, 97, 141, 180, 212, 235, 250, 254, 250, 235, 212, 180, 141, 97, 49 };
-
-static const uint8_t octave_delay[] = { 36, 34, 32, 31, 29, 27, 26, 24, 23, 22, 20, 19, 18, 17, 16, 15, 14, 14, 13, 12, 11, 11, 10, 10 };
+static const PROGMEM uint8_t sin[] = {0, 49, 97, 141, 180, 212, 235, 250, 254, 250, 235, 212, 180, 141, 97, 49 };
+static const PROGMEM uint8_t octave_delay[] = { 36, 34, 32, 31, 29, 27, 26, 24, 23, 22, 20, 19, 18, 17, 16, 15, 14, 14, 13, 12, 11, 11, 10, 10 };
+static const PROGMEM struct { uint8_t a; uint8_t b; } synth[] = { { 7, 6}, {7, 5}, {7,5}, {6,5} };
+static const PROGMEM uint8_t apreggiobase[] =	{ 3, 4, 4, 5 };
+static const PROGMEM uint8_t bassdrum[] =	{ 1, 1, 1, 1 };
+static const PROGMEM uint8_t snare[] = 	{ 1, 1, 1, 1 };
 
 static inline uint8_t next_rnd()
 {
@@ -33,18 +40,9 @@ static inline uint8_t next_sin(const uint8_t step)
 uint8_t next_sample()
 {
 static uint16_t t=0;
-
-
-
 static uint8_t t8=0;
 static uint8_t barevent=0;
 static uint8_t bars=0;
-
-static struct { uint8_t a; uint8_t b; } synth[] = 
-{ { 7, 6}, {7, 5}, {7,5}, {6,5} };
-static uint8_t apreggiobase[] =	{ 3, 4, 4, 5 };
-static uint8_t bassdrum[] =	{ 1, 1, 1, 1 };
-static uint8_t snare[] = 	{ 1, 1, 1, 1 };
 static uint8_t pc = 0;
 static uint8_t apreggiocnt = 1;
 
@@ -93,7 +91,7 @@ if(t % APREGGIO_DELAY == 0)
 
 // render synth
 
-synth1 = t&t>>(synth[pc].a) | t&t>>(synth[pc].b);
+synth1 = (t&(t>>(synth[pc].a))) | (t&(t>>(synth[pc].b)));
 synth1 += synth1;
 /*
 if(t == next_sin_time)
